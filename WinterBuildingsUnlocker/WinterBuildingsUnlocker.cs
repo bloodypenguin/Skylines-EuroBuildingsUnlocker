9964d842a7445d62434c6a60c5df99d1fb5c8a03
Egor Aralov <aei.omsk@gmail.com>
2017-03-01 14:03:37 +0600

    Add Winter Buildings Unlocker

---
+++ WinterBuildingsUnlocker/WinterBuildingsUnlocker.cs
@@ -0,0 +1,10 @@
+﻿using ICities;
+
+namespace WinterBuildingsUnlocker
+{
+    public class WinterBuildingsUnlocker : IUserMod
+    {
+        public string Name => "Winter Buildings Unlocker";
+        public string Description => "Brings Winter buildings to other environments and vice versa";
+    }
+}
